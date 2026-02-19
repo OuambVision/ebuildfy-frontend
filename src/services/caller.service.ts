370cde0a4ba052ed230a03603eb0af28e07097d8
OuambVision <ouambvision@gmail.com>
2026-02-19 15:38:45 -0500

    set api url

--- src/services/caller.service.ts
+++ src/services/caller.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_PAYLOAD_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Create an Axios instance with base configuration
 const Axios = axios.create({
